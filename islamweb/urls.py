68609997602deb87bb7b3f4573e3c358fd3ccc83
Your Name <you@example.com>
2025-05-21 13:55:13 +0000

    migration bug fix & manage .venv

--- islamweb/urls.py
+++ islamweb/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('infos.urls')),
-    path('zone-secrete-diamathereal/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('prieres/', include('prayers.urls')),
     path('grappelli/', include('grappelli.urls')),
 ]
